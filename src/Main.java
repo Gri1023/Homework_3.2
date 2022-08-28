d0ee18100fe0979ca840baab2b931a6360e08a00
bavykin.grisha <bavykin.grisha@syk.fi>
2022-08-28 15:43:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean shining = true;
+        int time = 16;
+        if (shining && (time>=10 && time<=16))
+            System.out.println("Please use sunscreen");
+    }
+}
